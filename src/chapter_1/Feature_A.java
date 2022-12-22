6fa34c471c22a630308436b0d87913663320d5a8
Sultan Orbay <orbaysultan@gmail.com>
2022-12-22 10:25:01 +0300

    added more feature

--- src/chapter_1/Feature_A.java
+++ src/chapter_1/Feature_A.java
@@ -3,5 +3,6 @@ package chapter_1;
 public class Feature_A {
     public static void main(String[] args) {
         System.out.println("For git needs more feature");
+        System.out.println("Created more line");
     }
 }
