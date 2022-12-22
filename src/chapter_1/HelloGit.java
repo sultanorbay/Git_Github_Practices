3d650a53462f2434b10373f60473ec7db64333d9
Sultan Orbay <orbaysultan@gmail.com>
2022-12-22 10:20:44 +0300

    Added second line

--- src/chapter_1/HelloGit.java
+++ src/chapter_1/HelloGit.java
@@ -3,5 +3,6 @@ package chapter_1;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("I don't like git");
     }
 }
