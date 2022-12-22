a0f688131a8f318b99cd8ef51bcc2b1a75bd2cd0
Sultan Orbay <orbaysultan@gmail.com>
2022-12-22 10:54:01 +0300

    done

--- src/chapter_1/Feature_B.java
+++ src/chapter_1/Feature_B.java
@@ -4,5 +4,6 @@ public class Feature_B {
     public static void main(String[] args) {
         System.out.println("feature b");
         System.out.println("Hello feature");
+        System.out.println("gsdhfjvkbnl");
     }
 }
