17cc1003e5f40bd1fa6e4c79980c73d100239bcd
Sultan Orbay <orbaysultan@gmail.com>
2022-12-22 10:34:37 +0300

    addedn new feature

--- src/chapter_1/Feature_B.java
+++ src/chapter_1/Feature_B.java
@@ -3,5 +3,6 @@ package chapter_1;
 public class Feature_B {
     public static void main(String[] args) {
         System.out.println("feature b");
+        System.out.println("Hello feature");
     }
 }
